238278469263ab06dfcda21b6e634c1867bd8704
IsraelNarvaezSaavedra <narvaez.saavedra.israel@iescamas.es>
2025-05-21 20:35:00 +0200

    DAOProducto

--- TrabajoFinal/src/main/java/bdd/ConexionBD.java
+++ TrabajoFinal/src/main/java/bdd/ConexionBD.java
@@ -8,8 +8,7 @@ import java.sql.SQLException;
 
 public class ConexionBD {
 public static Connection conectarBD() throws SQLException {
-        String url = "jdbc:oracle:thin:@localhost:1521:xe";
-        return DriverManager.getConnection(url, "BDgame", "BDgame");
+        return DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "BDgame", "BDgame");
     }
 
     public static void desconectarBD(Connection conn) {
