fb744c382fbab8521e586fa6d315000bdf1e9eec
josemiguelzerpagarcia <zerpa.garcia.jose.miguel@iescamas.es>
2025-05-22 16:42:00 +0200

    Login terminado

--- TrabajoFinal/src/main/java/interfaz/Login.java
+++ TrabajoFinal/src/main/java/interfaz/Login.java
@@ -4,12 +4,19 @@
  */
 package interfaz;
 
+import bdd.DAOPersona;
+import entidades.Persona;
+
 /**
  *
  * @author usuarioDAW
  */
 public class Login extends javax.swing.JFrame {
+
+    protected Main main;
     protected Registrarse registrarse;
+    protected DAOPersona daoPersona = new DAOPersona();
+
     /**
      * Creates new form Login
      */
@@ -31,7 +38,7 @@ public class Login extends javax.swing.JFrame {
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         nombreUs = new javax.swing.JTextField();
-        jPasswordField1 = new javax.swing.JPasswordField();
+        contraseñaUs = new javax.swing.JPasswordField();
         botonInicarSesion = new javax.swing.JButton();
         jLabel4 = new javax.swing.JLabel();
         botonRegistrarse = new javax.swing.JButton();
@@ -53,9 +60,9 @@ public class Login extends javax.swing.JFrame {
             }
         });
 
-        jPasswordField1.addActionListener(new java.awt.event.ActionListener() {
+        contraseñaUs.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jPasswordField1ActionPerformed(evt);
+                contraseñaUsActionPerformed(evt);
             }
         });
 
@@ -95,7 +102,7 @@ public class Login extends javax.swing.JFrame {
                             .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                             .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                         .addGap(26, 26, 26)
-                        .addComponent(jPasswordField1, javax.swing.GroupLayout.PREFERRED_SIZE, 299, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(contraseñaUs, javax.swing.GroupLayout.PREFERRED_SIZE, 299, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGap(19, 19, 19)
                         .addComponent(jLabel4)
@@ -118,7 +125,7 @@ public class Login extends javax.swing.JFrame {
                 .addGap(44, 44, 44)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jPasswordField1, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(contraseñaUs, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(50, 50, 50)
                 .addComponent(botonInicarSesion)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 112, Short.MAX_VALUE)
@@ -151,12 +158,22 @@ public class Login extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_nombreUsActionPerformed
 
-    private void jPasswordField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jPasswordField1ActionPerformed
+    private void contraseñaUsActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_contraseñaUsActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jPasswordField1ActionPerformed
+    }//GEN-LAST:event_contraseñaUsActionPerformed
 
     private void botonInicarSesionActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botonInicarSesionActionPerformed
-        // TODO add your handling code here:
+        if (daoPersona.existeCuentaPersona(contraseñaUs.getText(), nombreUs.getText())) {
+            if (main == null || !main.isDisplayable()) {
+                this.dispose();
+                main = new Main();
+                main.setVisible(true);
+            } else {
+                main.toFront();
+            }
+        } else {
+            Persona.UsuarioVacio("El usuario y la contraseña no coinciden", "Error");
+        }
     }//GEN-LAST:event_botonInicarSesionActionPerformed
 
     private void botonRegistrarseActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botonRegistrarseActionPerformed
@@ -208,12 +225,12 @@ public class Login extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton botonInicarSesion;
     private javax.swing.JButton botonRegistrarse;
+    private javax.swing.JPasswordField contraseñaUs;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JPasswordField jPasswordField1;
     private javax.swing.JTextField nombreUs;
     // End of variables declaration//GEN-END:variables
 }
